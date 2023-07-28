34243086997f128020a614edcdd20c0444fc62b3
quincytobsz <quincytobayiwa@gmail.com>
2023-07-28 18:04:14 +0200

    DONE

--- src/Componets/pages/nonlab.js
+++ src/Componets/pages/nonlab.js
@@ -1,36 +1,54 @@
-import React from 'react';
-import './Info.css';
 
+import React from "react";
+import './info.css'
 
-const nonlabs=()=> {
-return (
-    <div className='app'>
-<div className='container'>
-    <div>
-    <div>
-    <label className="labels">Country of Residence:</label><br />
-</div>
-  <div>
-  <label className="labels">Age:</label><br />
-</div>
-<div>
-    <label className="labels">Gender:</label><br />
-</div>
-<div>
-    <label className="labels">Smoking:</label><br />
-</div>  
-<div>
-    <label className="labels">Systolic BP:</label><br />
-</div>
-<div>
-    <label className="labels">BMI:</label>   <br />
-    
-</div>
+class nonlabs extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { 
+       formValues: [{ countryofresidence: "", age: "",gender:"",smoking:"",systolicBP:"",BMI:"" }]
+     };
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  
+  handleChange(i, e) {
+    let formValues = this.state.formValues;
+    formValues[i][e.target.name] = e.target.value;
+    this.setState({ formValues });
+  }
 
-    </div>
-    </div>
-    </div>
-  );
+  handleSubmit(event) {
+    event.preventDefault();
+    alert(JSON.stringify(this.state.formValues));
+  }
 
+  render() {
+
+    return (
+        <form  onSubmit={this.handleSubmit}>
+          {this.state.formValues.map((element, index) => (
+            <div className="form-inline" key={index}>
+              <label>Country of residence</label>
+              <input type="text" name="country of residence" value={element.countryofresidence || ""} onChange={e => this.handleChange(index, e)} />
+              <label>Age</label>
+              <input type="number" name="age" value={element.age || ""} onChange={e => this.handleChange(index, e)} />
+              <label>Gender</label>
+              <input type="text" name="gender" value={element.gender || ""} onChange={e => this.handleChange(index, e)} />
+              <label>Smoking</label>
+              <input type="text" name="smoking" value={element.smoking || ""} onChange={e => this.handleChange(index, e)} />
+              <label>Systolic BP</label>
+              <input type="text" name="systolic BP" value={element.systolicBP || ""} onChange={e => this.handleChange(index, e)} />
+              <label>BMI</label>
+              <input type="text" name="BMI" value={element.BMI || "Calculate BMI"} onChange={e => this.handleChange(index, e)} />
+             
+            </div>
+          ))}
+          <div className="button-section">
+              
+              <button className="button submit" type="submit">Calculate</button>
+          </div>
+      </form>
+    );
+  }
 }
 export default nonlabs;
--- src/index.js
+++ src/index.js
@@ -4,22 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {
-  createBrowserRouter,
+  createHashRouter,
   RouterProvider,
 } from "react-router-dom";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
-    path: "/",
+    path: "/*",
     element: <App/>,
   },
 
-])
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- 
+ <React.StrictMode>
    <RouterProvider router={router} />
-  
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
